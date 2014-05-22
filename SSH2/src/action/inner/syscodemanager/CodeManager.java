debe524d8496f541dd478e27f87c77eb76b1cf5c
L_Y_J <LYJ_LYJ@126.com>
2014-05-22 20:33:28 +0800

    1

--- SSH2/src/action/inner/syscodemanager/CodeManager.java
+++ SSH2/src/action/inner/syscodemanager/CodeManager.java
@@ -1,16 +1,24 @@
 package action.inner.syscodemanager;
 
+import bean.Department;
+import bean.Job;
 import com.opensymphony.xwork2.ActionSupport;
 import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
 import org.apache.struts2.interceptor.ServletRequestAware;
 import org.apache.struts2.interceptor.ServletResponseAware;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import service.DepartmentService;
+import sun.tools.jar.resources.jar_it;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
 
 /**
  * Created by yongjie on 14-5-9.
@@ -18,10 +26,16 @@ import java.util.HashMap;
 @Controller @Scope("prototype")
 public class CodeManager extends ActionSupport implements ServletRequestAware,ServletResponseAware {
 
+	@Resource
+	DepartmentService departmentService;
+
 	private HttpServletResponse response;
 	private HttpServletRequest request;
 	private String departmentId;
-	private String number;
+	private String departName;
+	private String data;
+	private String jobId;
+	private String jobName;
 
 	@Override
 	public void setServletRequest(HttpServletRequest httpServletRequest) {
@@ -41,29 +55,59 @@ public class CodeManager extends ActionSupport implements ServletRequestAware,Se
 		this.departmentId = departmentId;
 	}
 
-	public String getNumber() {
-		return number;
+	public String getDepartName() {
+		return departName;
 	}
 
-	public void setNumber(String number) {
-		this.number = number;
+	public void setDepartName(String departName) {
+		this.departName = departName;
+	}
+
+	public String getData() {
+		return data;
+	}
+
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	public String getJobId() {
+		return jobId;
+	}
+
+	public void setJobId(String jobId) {
+		this.jobId = jobId;
+	}
+
+	public String getJobName() {
+		return jobName;
+	}
+
+	public void setJobName(String jobName) {
+		this.jobName = jobName;
 	}
 
 	/**
-	 * 查询所有省级，市级，县级部门，级别使用number:one two three区分
+	 * 查询所有省级，市级，县级部门
 	 * @return
 	 */
 	public String SelectDepartmentOne(){
 		response.setContentType("json/javascript;charset=utf-8");
-		HashMap<String,String> map1 = new HashMap<String, String>();
-		HashMap<String,String> map2 = new HashMap<String, String>();
-		map1.put("id","1");
-		map1.put("name","人事部");
-		map2.put("id","2");
-		map2.put("name","后勤部");
 		JSONArray jsonArray = new JSONArray();
-		jsonArray.add(map1);
-		jsonArray.add(map2);
+
+		List list = departmentService.getDepartments();
+		if (list!=null){
+			for (Object o : list){
+				Department department = (Department) o;
+				if (department.getLeval().equals(this.data)){
+					HashMap<String,String> map = new HashMap<String, String>();
+					map.put("id",department.getId().toString());
+					map.put("name",department.getDepartmentName());
+					jsonArray.add(map);
+				}
+			}
+		}
+
 		try {
 			response.getWriter().println(jsonArray);
 		} catch (IOException e) {
@@ -79,16 +123,19 @@ public class CodeManager extends ActionSupport implements ServletRequestAware,Se
 	public String SelectDepartmentJob(){
 		response.setContentType("json/javascript;charset=utf-8");
 
-		HashMap<String,String> map1 = new HashMap<String, String>();
-		HashMap<String,String> map2 = new HashMap<String, String>();
-		map1.put("id","1");
-		map1.put("name","秘书");
-		map2.put("id","2");
-		map2.put("name","经理");
-
 		JSONArray jsonArray = new JSONArray();
-		jsonArray.add(map1);
-		jsonArray.add(map2);
+		Department department = departmentService.getDepartment(Integer.parseInt(this.departmentId));
+		Set jobs = department.getJob();
+		if (jobs!=null){
+			for (Object o : jobs){
+				Job j = (Job) o;
+				HashMap<String,String> map = new HashMap<String, String>();
+				map.put("id",j.getId().toString());
+				map.put("name",j.getJobName());
+				jsonArray.add(map);
+			}
+		}
+
 		try {
 			response.getWriter().println(jsonArray);
 		} catch (IOException e) {
@@ -96,4 +143,108 @@ public class CodeManager extends ActionSupport implements ServletRequestAware,Se
 		}
 		return null;
 	}
+
+
+	/**
+	 * 新增一个部门,departmentName data(表示级别)
+	 * return
+	 */
+	public String AddDepartment(){
+		response.setContentType("json/javascript;charset=utf-8");
+		JSONArray json = new JSONArray();
+		HashMap<String,String> map = new HashMap<String, String>();
+
+		Department department = new Department();
+		department.setDepartmentName(this.departName);
+		department.setLeval(this.data);
+		boolean b = departmentService.addDepartment(department);
+		if (b){
+			map.put("state","success");
+		}
+		else{
+			map.put("state","failed");
+		}
+		json.add(map);
+		try {
+			response.getWriter().println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+	/**
+	 * 删除部门
+	 * rturn
+	 */
+	public String DelDepartment(){
+		response.setContentType("json/javascript;charset=utf-8");
+		String tag = "failed";
+
+		Department department = departmentService.getDepartment(Integer.parseInt(this.departmentId));
+		if (department!=null){
+			boolean a = departmentService.deleteDepartment(department);
+			if (a)
+				tag = "success";
+		}
+
+		HashMap<String,String> map = new HashMap<String, String>();
+		map.put("state",tag);
+		JSONObject json = JSONObject.fromObject(map);
+		try {
+			response.getWriter().println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	/**
+	 * 添加职位
+	 * @return
+	 */
+	public String AddJob(){
+		response.setContentType("json/javascript;charset=utf-8");
+		String tag = "failed";
+
+		Department department = departmentService.getDepartment(Integer.parseInt(this.departmentId));
+		if (department!=null){
+			Job job = new Job();
+			job.setJobName(this.jobName);
+			department.getJob().add(job);
+			boolean a = departmentService.updateDepartment(department);
+			if (a)
+				tag = "success";
+		}
+
+		HashMap<String,String> map = new HashMap<String, String>();
+		map.put("state",tag);
+		JSONObject json = JSONObject.fromObject(map);
+		try {
+			response.getWriter().println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+
+	public String DelJob(){
+		response.setContentType("json/javascript;charset=utf-8");
+		String tag = "failed";
+
+
+
+		HashMap<String,String> map = new HashMap<String, String>();
+		map.put("state",tag);
+		JSONObject json = JSONObject.fromObject(map);
+		try {
+			response.getWriter().println(json);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
 }
--- SSH2/web/js/syscode.js
+++ SSH2/web/js/syscode.js
@@ -6,15 +6,29 @@ function close(){
     $("#divAddDepartment").hide();
 }
 
+function close2(){
+    $("#divAddJob").hide();
+}
+
 //查询各级部门，添加到左侧表格中
 var num = null;
 function SelectLevalOne(){
     num = $(this).attr('class').split(" ");
+    var data = null;
+    if (num[1]=="one"){
+        data="省级部门";
+    }
+    if (num[1]=="two"){
+        data="市级部门";
+    }
+    if (num[1]=="three"){
+        data="县级部门";
+    }
 
     $.ajax({
         url: 'department_one.do',
         type: 'POST',
-        data: {'number':num[1]},
+        data: {'data':data},
         dataType: 'text',
         success: function(data){
 //            alert(data);
@@ -50,6 +64,7 @@ function Tbody1Selected(){
         success: function(data){
 //            alert(data);
             var tbody2 = $("#tbody2");
+            tbody2.children().remove();
             var json = $.parseJSON(data);
             for (var i=0; i<json.length; i++){
                 var tr = $("<tr class='info'></tr>");
@@ -82,8 +97,162 @@ function AddDepartment(){
     }
 
     $("#divAddDepartment").show(150);
-//    var departmentName = $("#departmentName").val();
+
+}
+
+//提交数据，保存新增部门
+function SaveDepartment(){
+    $("#divAddDepartment").hide();
+
+    var departmentName = $("#departmentName").val();
 //    alert(departmentName);
+    var data = null;
+    if (num[1]=="one"){
+        data="省级部门";
+    }
+    if (num[1]=="two"){
+        data="市级部门";
+    }
+    if (num[1]=="three"){
+        data="县级部门";
+    }
+
+    $.ajax({
+        url: 'add_department.do',
+        type: 'POST',
+        data:{'departName':departmentName,'data':data},
+        dataType:'text',
+        success:function(data){
+//            alert(data);
+            var json = $.parseJSON(data);
+            if (json[0].state=="success"){
+                $("#divAlert").attr('class','alert alert-success');
+                $("#divAlert").children('p').html("<h4>部门添加成功</h4>");
+                $("#divAlert").show();
+            }
+            else{
+                $("#divAlert").attr('class','alert alert-danger');
+                $("#divAlert").children('p').html("<h4>部门添加失败</h4>");
+                $("#divAlert").show();
+            }
+        }
+    });
+
+}
+
+function EditDepartment(){
+    if (department==null){
+        $("#divAlert").attr('class','alert alert-danger');
+        $("#divAlert").children('p').html("<h4>请先选择部门</h4>");
+        $("#divAlert").show();
+    }
+}
+
+function DeleteDepartment(){
+    if (department==null){
+        $("#divAlert").attr('class','alert alert-danger');
+        $("#divAlert").children('p').html("<h4>请先选择部门</h4>");
+        $("#divAlert").show();
+    }
+
+    var departmentId = department.children().html();
+//    alert(departmentId);
+
+    $.ajax({
+        url:'del_department.do',
+        type:'POST',
+        data:{'departmentId':departmentId},
+        dataType:'text',
+        success: function(data){
+//            alert(data);
+            var json = $.parseJSON(data);
+            if (json.state=="success"){
+                $("#divAlert").attr('class','alert alert-success');
+                $("#divAlert").children('p').html("<h4>部门删除成功</h4>");
+                $("#divAlert").show();
+                department.remove();
+                department = null;
+
+            }
+            else{
+                $("#divAlert").attr('class','alert alert-danger');
+                $("#divAlert").children('p').html("<h4>部门删除失败</h4>");
+                $("#divAlert").show();
+            }
+        }
+    });
+
+
+}
+
+function AddJob(){
+    if (department==null){
+        $("#divAlert").attr('class','alert alert-danger');
+        $("#divAlert").children('p').html("<h4>请选择部门，再进行添加</h4>");
+        $("#divAlert").show();
+        return;
+    }
+
+    $("#divAddJob").show();
+}
+
+function SaveJob(){
+    var jobName = $("#jobName").val();
+    if (jobName==null | jobName==""){
+        $("#divAlert").attr('class','alert alert-danger');
+        $("#divAlert").children('p').html("<h4>请填写岗位名称</h4>");
+        $("#divAlert").show();
+        return;
+    }
+
+    $("#divAddJob").hide();
+    var departmentId = department.children().html();
+
+    $.ajax({
+        url:'add_job.do',
+        type:'POST',
+        data:{'departmentId':departmentId,'jobName':jobName},
+        dataType:'text',
+        success: function(data){
+//            alert(data);
+            var json = $.parseJSON(data);
+            if (json.state=="success"){
+                $("#divAlert").attr('class','alert alert-success');
+                $("#divAlert").children('p').html("<h4>岗位添加成功</h4>");
+                $("#divAlert").show();
+            }
+            else{
+                $("#divAlert").attr('class','alert alert-danger');
+                $("#divAlert").children('p').html("<h4>岗位添加失败</h4>");
+                $("#divAlert").show();
+            }
+        }
+    });
+}
+
+
+//删除岗位
+function DeleteJob(){
+    if (job==null){
+        $("#divAlert").attr('class','alert alert-danger');
+        $("#divAlert").children('p').html("<h4>请选中需要删除的岗位</h4>");
+        $("#divAlert").show();
+        return;
+    }
+
+    var departmentId = department.children().html();
+    var jobId = job.children().html();
+
+    $.ajax({
+        url:'',
+        type:'POST',
+        data:{'departmentId':departmentId,'jobId':jobId},
+        dataType:'text',
+        success:function(data){
+            alert(data);
+        }
+    });
+
 }
 
 
@@ -94,9 +263,17 @@ function Init(){
             zIndex: "1000",
             backgroundColor: "#F8F8F8"
         }).hide();
+    $("#divAddJob").css(
+        {   position: "absolute",
+            top: "100px",left: "300px",right: "400px",bottom: "200px",
+            zIndex: "1000",
+            backgroundColor: "#F8F8F8"
+        }).hide();
+
     $("#divAlert").css(
         {   position: "absolute",
-            top: "100px",left: "500px",right: "400px",bottom: "400px"
+            top: "100px",left: "500px",right: "400px",bottom: "400px",
+            zIndex:1000
         }).hide();
 
     $("#close1").click(close);
@@ -113,4 +290,29 @@ function Init(){
     $(".info").live('click',Tbody2Selected);
     //添加部门
     $("#addDepartment").click(AddDepartment);
+    //提交数据，保存添加的部门
+    $("#save").click(SaveDepartment);
+    //编辑选中的部门
+    $("#editDepartment").click(EditDepartment);
+    //删除选中的部门
+    $("#delDepartment").click(DeleteDepartment);
+
+    //添加岗位时弹窗
+    $("#addJob").click(AddJob);
+    $("#close3").click(close2);
+    $("#close4").click(close2);
+    //提交数据，保存新增的岗位
+    $("#savejob").click(SaveJob);
+    //删除岗位的响应函数
+    $("#delJob").click(DeleteJob);
+
 }
+
+
+
+
+
+
+
+
+
